0fa719f0cdf0e6cbe865435a8e555df8b46976a1
akilakavinda99 <akilakavinda909@gmail.com>
2023-03-11 08:16:19 +0530

    testing firebase connection

--- SLIITCommunity/src/screens/Login.js
+++ SLIITCommunity/src/screens/Login.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import {Text, View, Button} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
 
 const Login = () => {
   const navigation = useNavigation();
   const goToReg = () => {
-    navigation.navigate('Register');
+    // navigation.navigate('Register');
+
+    firestore()
+      .collection('Users')
+      .add({
+        name: 'Ada Lovelace',
+        age: 30,
+      })
+      .then(() => {
+        console.log('User added!');
+      });
   };
   return (
     <View>
