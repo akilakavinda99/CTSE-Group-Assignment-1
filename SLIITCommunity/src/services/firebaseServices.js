8a4158f33a50edee33a32adb35c84c63e06123ae
Sachintha Gunaratne <sachinthasl99@gmail.com>
2023-03-14 13:17:25 +0530

    update firebaseServices.js

--- SLIITCommunity/src/services/firebaseServices.js
+++ SLIITCommunity/src/services/firebaseServices.js
@@ -12,6 +12,82 @@ export const checkForDocument = async (collectionName, docId) => {
   }
 };
 
+export const addDocument = async (collectionName, data) => {
+  try {
+    const addDoc = await firestore()
+      .collection(collectionName)
+      .add(data);
+    return addDoc;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+export const getDocuments = async (collectionName) => {
+  try {
+    const getDocs = await firestore()
+      .collection(collectionName)
+      .get();
+    return getDocs.docs.map((doc) => doc.data());
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+export const getDocument = async (collectionName, docId) => {
+  try {
+    const getDoc = firestore()
+      .collection(collectionName)
+      .doc(docId)
+      .get();
+    return getDoc.data();
+  } catch (error) {
+    console.log(error);
+    return {};
+  }
+};
+
+export const updateDocument = async (collectionName, docId, data) => {
+  try {
+    const updateDoc = await firestore()
+      .collection(collectionName)
+      .doc(docId)
+      .update(data);
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export const deleteDocument = async (collectionName, docId) => {
+  try {
+    const deleteDoc = await firestore()
+      .collection(collectionName)
+      .doc(docId)
+      .delete();
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export const getDocumentsByField = async (collectionName, fieldName, value) => {
+  try {
+    const getDocs = await firestore()
+      .collection(collectionName)
+      .where(fieldName, '==', value)
+      .get();
+    return getDocs.docs.map((doc) => doc.data());
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
 export const addDocumentWithCustomID = async (
   collectionName,
   customId,
