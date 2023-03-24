2493fac02f11fd7bd1c94a21739a380df23aea86
Sachintha Gunaratne <sachinthasl99@gmail.com>
2023-03-25 01:12:05 +0530

    ui changes

--- SLIITCommunity/src/screens/Profile/MyCommunities.js
+++ SLIITCommunity/src/screens/Profile/MyCommunities.js
@@ -2,28 +2,47 @@ import React, { useEffect, useState } from 'react';
 import { View, RefreshControl, SafeAreaView, ScrollView, StyleSheet, Text, Dimensions } from "react-native";
 import Loading from '../../components/commonComponents/loading';
 import CommunityCard from '../../components/communities/communityCard';
-import {  getDocumentsByFieldWithId } from '../../services/firebaseServices';
+import { getDocumentsByFieldWithId } from '../../services/firebaseServices';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { primaryColors } from '../../styles/colors';
 import AppLoader from '../../components/commonComponents/AppLoader';
+import SearchBar from "react-native-dynamic-search-bar";
+import * as Animatable from 'react-native-animatable';
+import { getDataFromAsync } from '../../constants/asyncStore';
+import asyncStoreKeys from '../../constants/asyncStoreKeys';
 
 const MyCommunities = () => {
   const [communities, setCommunities] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [showingCommunities, setShowingCommunities] = useState([]);
 
   const getCommunities = () => {
-    getDocumentsByFieldWithId('communities', 'itNumber', 'IT20206284')
+    getDataFromAsync(asyncStoreKeys.IT_NUMBER)
       .then((res) => {
-        setCommunities(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => {
-        setLoading(false);
+        getDocumentsByFieldWithId('communities', 'itNumber', res)
+          .then((res) => {
+            setCommunities(res);
+            setShowingCommunities(res);
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+          .finally(() => {
+            setLoading(false);
+          });
       });
   };
+
+  const onSearch = (text) => {
+    const filteredCommunities = communities.filter((communities) => {
+      return communities.title.toLowerCase().includes(text.toLowerCase()) ||
+        communities.faculty.toLowerCase().includes(text.toLowerCase()) ||
+        communities.description.toLowerCase().includes(text.toLowerCase());
+    });
+
+    setShowingCommunities(filteredCommunities);
+  }
 
   const onRefresh = () => {
     setRefreshing(true);
@@ -39,9 +58,17 @@ const MyCommunities = () => {
 
   return (
     <SafeAreaView style={styles.mainView}>
+      <Animatable.View animation="pulse" iterationCount={"infinite"} direction="alternate">
+        <SearchBar
+          placeholder="Search Communities"
+          style={{ marginTop: 10, marginBottom: 10 }}
+          onChangeText={onSearch}
+        />
+      </Animatable.View>
       {loading ? (
         <AppLoader />
       ) : (
+
         <ScrollView
           style={styles.scrollView}
           refreshControl={
@@ -55,28 +82,32 @@ const MyCommunities = () => {
             }
           ]}
         >
+
           {communities.length > 0 ? (
-            <View style={{ flexWrap: 'wrap', flexDirection: 'row' }}>
-              {communities.map((community, index) => {
-                return (
-                  <CommunityCard
-                    key={index}
-                    communities={community}
-                    style={{ width: windowWidth / 2 }}
-                  />
-                );
-              })}
+            <View style={{ flexWrap: 'wrap', flexDirection: 'row', marginBottom: 80 }}>
+              {loading ? <AppLoader /> :
+                showingCommunities.map((community, index) => {
+                  return (
+                    <CommunityCard
+                      key={index}
+                      communities={community}
+                      style={{ width: windowWidth / 2 }}
+                    />
+                  );
+                })}
             </View>
           ) : (
-            <View style={styles.emptyView}>
-              <Text style={styles.emptyText}>Oops!</Text>
-              <Text style={styles.emptyText}>No communities available</Text>
-              <Ionicons
-                name="ios-sad-outline"
-                size={39}
-                color="#58595a"
-                style={{ marginTop: 10 }}
-              />
+            <View style={[styles.emptyView]}>
+              <Animatable.Text animation="slideInDown" iterationCount={"1"} direction="alternate" style={styles.emptyText}>Oops!</Animatable.Text>
+              <Animatable.Text animation="slideInDown" iterationCount={"1"} direction="alternate" style={styles.emptyText}>No communities available</Animatable.Text>
+              <Animatable.Text animation="slideInDown" iterationCount={"1"} direction="alternate">
+                <Ionicons
+                  name="ios-sad-outline"
+                  size={39}
+                  color="#58595a"
+                  style={{ marginTop: 10 }}
+                />
+              </Animatable.Text>
             </View>
           )}
         </ScrollView>
@@ -87,7 +118,7 @@ const MyCommunities = () => {
 
 const styles = StyleSheet.create({
   mainView: {
-    backgroundColor: "#cae3eb",
+    // backgroundColor: "#cae3eb",
     height: "100%",
   },
   scrollView: {
--- SLIITCommunity/src/screens/Profile/MyNotices.js
+++ SLIITCommunity/src/screens/Profile/MyNotices.js
@@ -6,6 +6,11 @@ import NoticeCard from '../../components/notices/noticeCard';
 import { getDocumentsByFieldWithId } from '../../services/firebaseServices';
 import { primaryColors } from '../../styles/colors';
 import NetCheck from '../../components/commonComponents/netCheck';
+import HorizontalLine from '../../components/commonComponents/horizontalLine';
+import * as Animatable from 'react-native-animatable';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { getDataFromAsync } from '../../constants/asyncStore';
+import asyncStoreKeys from '../../constants/asyncStoreKeys';
 
 const MyNotices = () => {
     const [notices, setNotices] = useState([]);
@@ -15,17 +20,20 @@ const MyNotices = () => {
 
     const getNotices = async () => {
         setRefreshing(true);
-        getDocumentsByFieldWithId('notices', 'owner', 'IT20206284')
+        getDataFromAsync(asyncStoreKeys.IT_NUMBER)
             .then((res) => {
-                setNotices(res);
-                setShowingNotices(res);
-                // console.log(res);
-                setRefreshing(false);
+                getDocumentsByFieldWithId('notices', 'owner', res)
+                    .then((res) => {
+                        setNotices(res);
+                        setShowingNotices(res);
+                        // console.log(res);
+                        setRefreshing(false);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        setRefreshing(false);
+                    });
             })
-            .catch((err) => {
-                console.log(err);
-                setRefreshing(false);
-            });
 
     }
 
@@ -58,25 +66,46 @@ const MyNotices = () => {
                         onClearPress={() => onSearch("")}
                         placeholderTextColor={"#8e8e8e"}
                     />
-                    <ScrollView
-                        style={styles.scrollView}
-                        refreshControl={
-                            <RefreshControl
-                                refreshing={refreshing}
-                                getNotices={getNotices}
-                            />
-                        }
-                    >
-                        {
-                            // refreshing ? <Loading /> :
-                            showingNotices.map((notice, index) => {
-                                return (
-                                    <NoticeCard key={index} notice={notice} />
-                                )
-                            })
-                        }
-                        <View style={{ height: 90 }} />
-                    </ScrollView>
+                    {showingNotices.length == 0 ?
+                        <View style={[styles.scrollView, {
+                            flex: 1,
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }]}>
+                            <Animatable.Text animation="slideInDown" iterationCount={1} direction="alternate" style={styles.emptyText}>Oops!</Animatable.Text>
+                            <Animatable.Text animation="slideInDown" iterationCount={1} direction="alternate" style={styles.emptyText}>No notice available</Animatable.Text>
+                            <Animatable.Text animation="slideInDown" iterationCount={1} direction="alternate">
+                                <Ionicons
+                                    name="ios-sad-outline"
+                                    size={39}
+                                    color="#58595a"
+                                    style={{ marginTop: 10 }}
+                                />
+                            </Animatable.Text>
+                        </View> :
+                        <ScrollView
+                            style={styles.scrollView}
+                            refreshControl={
+                                <RefreshControl
+                                    refreshing={refreshing}
+                                    getNotices={getNotices}
+                                />
+                            }
+                        >
+                            {
+                                // refreshing ? <Loading /> :
+                                showingNotices.map((notice, index) => {
+                                    return (
+                                        <View key={index}>
+                                            <NoticeCard notice={notice} />
+                                            <HorizontalLine />
+                                        </View>
+                                    )
+                                })
+                            }
+                            <View style={{ height: 90 }} />
+                        </ScrollView>
+                    }
                 </>
             }
         </SafeAreaView>
@@ -85,7 +114,7 @@ const MyNotices = () => {
 
 const styles = StyleSheet.create({
     mainView: {
-        backgroundColor: primaryColors.background,
+        backgroundColor: primaryColors.primaryBlue,
         height: "100%",
         paddingTop: 10,
     },
@@ -93,6 +122,15 @@ const styles = StyleSheet.create({
         width: "100%",
         paddingHorizontal: 16,
         marginTop: 10,
+        backgroundColor: "#fff",
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+    },
+    emptyText: {
+        fontSize: 20,
+        fontWeight: 900,
+        color: primaryColors.darkGrey,
+        marginTop: 5,
     },
 });
 
